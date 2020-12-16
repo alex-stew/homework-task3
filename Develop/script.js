fdf0efccedd5aba8ccc1abf60541aa3cda44c9a2
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2020-12-16 16:12:24 +1030

    trying to fix loop

--- Develop/script.js
+++ Develop/script.js
@@ -2,26 +2,49 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(){
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-function userInputCriteria(){
+//user selected criteria and error messaging for incorrect inputs//
+function generatePassword(){
   var length = prompt("How long would you like your new password to be? ");
   if (length < 8 || length >128){
     alert("You have choosen an invalid length for your new password, please input a number between 8 and 128.");
-    return""
+    return (" ");
   }
-  else {
-    var lowerCase = confirm("Include lower-case letters? ");
-    var upperCase = confirm("Include upper-case letters? ");
-    var special = confirm("Include special characters? ");
-    var numeric = confirm("include numerical characters? ");
+  var lowerCase = confirm("Include lower-case letters? ");
+  var upperCase = confirm("Include upper-case letters? ");
+  var special = confirm("Include special characters? ");
+  var numeric = confirm("include numerical characters? ");
+  
+  if(!upperCase && !lowerCase && !special && !numeric){
+    alert("Ensure to select SOME character sets for me to work with...what do you want a password made of air? ");
   }
-}
+
+// adding the selected user inputs to a string variable based on confirm//
+  var charSelect = (" ");
+    if (upperCase){
+      charSelect += ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+    else if (lowerCase){
+      charSelect += ("abcdefghijklmnopqrxtuvyxyz");
+    }
+    else if (special){
+      charselect += ("#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+    }
+    else if (numeric){
+      charSelect += ("0123456789")
+    }
+// adding a loop to use the charSelect variable to randomly select for its avaliable list of characters and add them until they reach the determined password length//
+  for (var i=0; i<length; i++){
+    var randomNum = Math.floor(Math.random() * charSelect.length+1)
+  }
+password += charSelect[randomNum]; return password
+  }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", userInputCriteria);
+generateBtn.addEventListener("click", generatePassword);
